f8ea7f25d8a7bfed52f01e1ad7a5b407541b8939
nanaefuadaisie <nanaefuadaisie96@gmail.com>
2023-07-15 03:57:29 -0700

    0-positive_or_negative.c

--- 0x01-variable_if_else_while/0-positive_or_negative.c
+++ 0x01-variable_if_else_while/0-positive_or_negative.c
@@ -16,11 +16,17 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 
 	if (n > 0)
+	{
 		printf("%d is positive\n", n);
-	else if (n < 0)
+	}
+	else if (n == 0)
+	{
 		printf("%d is negative\n", n);
-	else
+	}
+	else{
 		printf("%d is zero\n", n);
 
+	
+	}
 	return (0);
 }
