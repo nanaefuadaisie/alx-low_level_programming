473467fdb255274073c6b5728d6888b61d0f8cd7
nanaefuadaisie <nanaefuadaisie96@gmail.com>
2023-07-15 19:37:51 -0700

    1-last_digit.c

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include <time.h>
+#include <studio.h>
+
+/**
+ * main - Prints the alphabet in lowercase, and then in uppercase
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int n;
+	int y;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	y = n % 10;
+	print("Last digit of %d is %d ", n, y);
+	if (y > 5)
+	{
+		printf("and is greater than 5");
+	}
+
+	if (y == 0)
+	{
+		printf("and is 0);
+	}
+	if (y < 6 && y != 0)
+	{
+		printf("and is less than 6 and not 0);
+	}
+
+	printf("\n");
+
+	return (0);
+}
