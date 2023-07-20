4c274da29337186fb73df9673dcbc3d0a6b1b5d7
nanaefuadaisie <nanaefuadaisie96@gmail.com>
2023-07-19 17:39:51 -0700

    main.h and readme files

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,7 +1,8 @@
-#ifndef HEADER_FILE
-#define HEADER_FILE
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
 
-int _putchar(char);
+int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
@@ -13,5 +14,7 @@ void jack_bauer(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int n);
+
+
 
 #endif
