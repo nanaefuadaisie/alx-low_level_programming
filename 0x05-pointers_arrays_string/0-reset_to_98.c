50e35b4a95de9ad29f59bed77b5b65c60ad01570
nanaefuadaisie <nanaefuadaisie96@gmail.com>
2023-07-24 06:26:09 -0700

    0-reset_to_98.c

--- 0x05-pointers_arrays_string/0-reset_to_98.c
+++ 0x05-pointers_arrays_string/0-reset_to_98.c
@@ -3,7 +3,8 @@
 /**
  * reset_to_98 - updates the value of the variable
  * the pointer points to to 98
- * @n: pointer to the variable to update
+ * @n: pointer to the variable to update to 98
+ * Return: Always 0
  */
 void reset_to_98(int *n)
 {
